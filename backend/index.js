797f0bc726d4a6e0232f81e353390a1473d6a83f
Rudra <u24ai068@aid.svnit.ac.in>
2025-08-30 20:53:47 +0530

    Change Detected

--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,17 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-dotenv.config();
+
 import './config/cloudinary.js';
 import { uploadImageToCloudinary } from './controllers/upload.controller.js';
-
-
-dotenv.config();
+import { createProxyMiddleware } from 'http-proxy-middleware';
+import { predictMangrove } from './controllers/predict.controller.js';
 import {connectDB} from './config/db.js';
 import userRouter from "./routes/userRoutes.js";
 import reportRouter from "./routes/reportRoutes.js";
 import LeaderboardRouter from "./routes/leaderboardRoutes.js";
 
-
+dotenv.config();
 const app = express();
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
@@ -26,6 +25,13 @@ app.use('/api', LeaderboardRouter)
 
 
 app.post('/api/predict', predictMangrove);
+
+app.use('/api/predict', createProxyMiddleware({
+  target: 'http://127.0.0.1:5000',
+  changeOrigin: true,
+  pathRewrite: { '^/api/predict': '/predict' },
+}));
+
 
 
 const PORT = process.env.PORT || 3000;
